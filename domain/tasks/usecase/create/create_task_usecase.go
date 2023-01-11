4649ebf8abdf5feaf035358a78605de11482d48a
Patricia <olli.so17@gmail.com>
2023-01-10 22:53:11 -0300

    feat(usecase):update usecase

--- domain/tasks/usecase/create/create_task_usecase.go
+++ domain/tasks/usecase/create/create_task_usecase.go
@@ -3,17 +3,18 @@ package create
 import (
 	"tasks_go/domain/tasks/entity"
 	"tasks_go/domain/tasks/repository"
+	"tasks_go/domain/tasks/usecase/create/dto"
 )
 
 type UseRepository struct {
 	repository.TaskRepository
 }
 
-func CreateTaskUseCase(input *InputTaskDto) (*OutPutTaskDto, error) {
-	u := UseRepository{}
+func CreateTaskUseCase(input *dto.InputTaskDto) (*dto.OutPutTaskDto, error) {
+	// u := UseRepository{}
 	t := &entity.Task{}
 	task, _ := t.NewTask(input.Title, input.Description)
-	newTask, _ := u.AddTask(task)
+	// newTask, _ := u.AddTask(task)
 
-	return &OutPutTaskDto{Task: *newTask}, nil
+	return &dto.OutPutTaskDto{Task: *task}, nil
 }
--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"tasks_go/domain/tasks/usecase/create"
+	"tasks_go/domain/tasks/usecase/create/dto"
 )
 
 func main() {
-	input := create.InputTaskDto{
+	input := dto.InputTaskDto{
 		Title: "teste", Description: "testando para ver se funciona",
 	}
 	task, err := create.CreateTaskUseCase(&input)
