2616139f06b94b6cf4c8124c3bb67bd5e645a717
Patricia <olli.so17@gmail.com>
2023-01-18 18:27:02 -0300

    feat(wire):create wire_gen

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -5,19 +5,27 @@ import (
 	"fmt"
 	"tasks_go/internal/infra/web/webserver"
 
-	_ "github.com/mattn/go-sqlite3"
+	// _ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "data.db")
+	// configs, err := configs.LoadConfig(".")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	db, err := sql.Open("postgres", "host=localhost port=54322 user=root password=root dbname=root sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
+
 	defer db.Close()
 	// creatreTaskUseCase := NewCreateTaskUseCase(db)
 	webserver := webserver.NewWebServer(":8080")
-	newWebTaskHandler := NewWebTaskHandler(db)
-	webserver.AddHandler("/task", newWebTaskHandler.Create)
+	newWebTaskHandler := NewWebTaskHandlerGen(db)
+
+	webserver.AddHandler("/tasks", newWebTaskHandler.Create)
 	fmt.Println("Starting web server on port", ":8080")
 	webserver.Start()
 
