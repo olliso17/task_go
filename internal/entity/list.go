d1616cf355070e9b1bcdb0d54c87f0bf4f828203
Patricia Silva <patriciasilva@deso-se.com.br>
2023-01-30 13:21:56 -0300

    feat(list): update entity list

--- internal/entity/list.go
+++ internal/entity/list.go
@@ -9,7 +9,6 @@ import (
 type ListEntity struct {
 	ID        string
 	Name      string
-	Tasks     []*Task
 	CreatedAt string
 	UpdatedAt string
 	DeletedAt string
@@ -29,13 +28,4 @@ func NewListEntity(name string) (*ListEntity, error) {
 	}
 
 	return list, nil
-}
-
-func AddTask(task *Task) (*ListEntity, error) {
-	var tasks []*Task
-	tasks = append(tasks, task)
-
-	return &ListEntity{
-		Tasks: tasks,
-	}, nil
 }
