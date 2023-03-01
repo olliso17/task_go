b99fcec16bc40345bd806ab78f2a11360d4a6fa7
Patricia Silva <patriciasilva@deso-se.com.br>
2023-03-01 09:10:58 -0300

    feat(front_end): create services

---
+++ front_end/services/list.ts
@@ -0,0 +1,44 @@
+import api from "./backend";
+
+
+
+export interface CreateListInputDto {
+    name: string
+}
+
+export interface OutputCreateListDto {
+    id: string
+    name: string
+ 
+}
+
+const getListAll = async () => {
+    const url = `lists`
+
+    const res = await api.get(url);
+
+    const data = res.data;
+
+    return data
+
+}
+
+const getListId = async (listId: string) => {
+
+    const url = `list/${listId}`
+
+    const res = await api.get(url);
+
+    const data = res.data;
+
+    return data
+};
+
+
+const postList = async (input: CreateListInputDto): Promise<OutputCreateListDto> => {
+    const url = `list/create`
+
+    const res = await api.post(url, input);
+
+    return res.data
+}
