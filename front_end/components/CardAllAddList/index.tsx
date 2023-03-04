cae6b30c1db85b9fce2149ccb6afa6b0292157c0
Patricia <olli.so17@gmail.com>
2023-03-03 22:35:29 -0300

    feat(front_end): creat add list

--- front_end/components/CardAllAddList/index.tsx
+++ front_end/components/CardAllAddList/index.tsx
@@ -1,15 +1,23 @@
+import { postList } from "@/services/handler/list_handler"
+import { useState } from "react"
+import { useMutation } from "react-query"
 import Inputs from "../Input"
 import TooltipTask from "../Tooltip"
 
 const CardAllAddList = () => {
+    const [name, setName] = useState('');
+    const mutation = useMutation({mutationFn:postList, onSuccess:() =>{
+        alert('create list successfully added')
+    }})
+    const onCreateList = (e) => {
+        e.preventDefault()
+        mutation.mutate({ name })
+        setName('')
+    }
     return (
-        <div className="flex flex-col m-2 justify-between h-auto items-center">
-            <Inputs style=" w-full p-2 flex flex-col justify-start " type='text' nameList="Name List" htmlFor="nameList" id="nameList" defaultValue='"Add Name List"' />
 
-            <div className="flex w-11/12 items-end justify-end">
-                <button className="bg-violet-900 mt-4 w-1/3 p-1 rounded-xl hover:bg-violet-700 t text-white">Save</button>
-            </div>
-        </div>
+        <Inputs onSubmit={onCreateList} onChange={(e) => setName(e.target.value)}  value={name} style=" w-full p-2 flex flex-col justify-start " type='text' nameList="Name List" htmlFor="nameList" id="nameList" defaultValue='"Add Name List"' />
+
     )
 }
 export default CardAllAddList
--- front_end/components/Input/index.tsx
+++ front_end/components/Input/index.tsx
@@ -1,3 +1,5 @@
+import { ChangeEventHandler } from "react"
+import { FormEventHandler } from "react"
 
 interface Props {
     nameList: string
@@ -6,17 +8,27 @@ interface Props {
     defaultValue: string
     type: string
     style: string
+    value: string
+    onSubmit: FormEventHandler<HTMLFormElement>
+    onChange: ChangeEventHandler<HTMLInputElement>
 }
 
-export default function Inputs({style, nameList, htmlFor, id, defaultValue, type }: Props) {
+export default function Inputs({ onChange, onSubmit, value, style, nameList, htmlFor, id, defaultValue, type }: Props) {
     return (
+        <div className="flex flex-col m-2 justify-between h-auto items-center">
+            <form onSubmit={onSubmit} className="flex flex-col w-80 ">
+                <label className="text-sm font-bold" htmlFor={htmlFor}>
+                    {nameList}
+                </label>
+                <input onChange={onChange} className="h-9 p-2 text-sm text-violet-400 border-2 border-opacity hover:border-violet-800 shadow-md rounded-md" type={type} id={id} value={value}
+                />
+                <div className="flex w-11/12 items-end justify-end">
+                    <button type="submit" className="bg-violet-900 mt-4 w-1/3 p-1 rounded-xl hover:bg-violet-700 t text-white">Save</button>
+                </div>
+            </form>
 
-        <div className="flex flex-col w-80 ">
-            <label className="text-sm font-bold" htmlFor={htmlFor}>
-                {nameList}
-            </label>
-            <input className="h-9 p-2 text-sm text-violet-400 border-2 border-opacity hover:border-violet-800 shadow-md rounded-md" type={type} id={id} defaultValue={defaultValue} />
         </div>
+
 
     )
 }
