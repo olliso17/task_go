8755da2dc2817a8edbdc1707d112854b6b34deaa
Patricia Silva <patriciasilva@deso-se.com.br>
2023-03-06 11:53:00 -0300

    feat(front_end): update list all

--- front_end/components/ListAll/index.tsx
+++ front_end/components/ListAll/index.tsx
@@ -22,7 +22,7 @@ const ListAll = () => {
         //     </div>
 
         // ))
-        <Flex className="flex-col">
+        <Flex className="flex-col overflow-auto w-full h-[680px]">
             <Flex className="bg-white rounded-2xl p-2 flex-col items-end m-1">
                 <Accordion className="bg-violet-500 text-violet-100 rounded-2xl container" defaultIndex={[0]} allowMultiple>
                     <AccordionItem>
@@ -77,7 +77,33 @@ const ListAll = () => {
                     icon={<AiFillDelete />}
                 />
             </Flex>
-
+            <Flex className="bg-white rounded-2xl p-2 flex-col items-end">
+                <Accordion className="bg-violet-500 text-violet-100 rounded-2xl container" defaultIndex={[0]} allowMultiple>
+                    <AccordionItem>
+                        <h2>
+                            <AccordionButton>
+                                <Box as="span" flex='1' textAlign='left'>
+                                    Section 1 title
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4} className="p-2">
+                            <Card className="p-2 m-1">Tasks</Card>
+                            <Card className="p-2 m-1">Tasks</Card>
+                            <Card className="p-2 m-1">Tasks</Card>
+                            <Card className="p-2 m-1">Tasks</Card>
+                        </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
+                <IconButton
+                    className="w-3/12 m-2"
+                    variant='outline'
+                    colorScheme='purple'
+                    aria-label='Send email'
+                    icon={<AiFillDelete />}
+                />
+            </Flex>
         </Flex>
 
 
