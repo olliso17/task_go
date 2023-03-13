5f666ef042292aa74722e5b0e2060b73caf1fc8f
Patricia Silva <patriciasilva@deso-se.com.br>
2023-03-13 13:21:00 -0300

    feat(login): update login

--- back_end/internal/infra/database/login_repository.go
+++ back_end/internal/infra/database/login_repository.go
@@ -16,7 +16,7 @@ func NewLoginRepository(db *sql.DB) *LoginRepository {
 	}
 }
 
-func (r *LoginRepository) Create(login *entity.Login) error {
+func (r *LoginRepository) Execute(login *entity.Login) error {
 	stmt, err := r.Db.Prepare("INSERT INTO logins (id, access_token, user_id, created_at, expired_at, is_expired) VALUES ($1, $2, $3, $4, $5, $6)")
 	if err != nil {
 		fmt.Print("o erro:", err)
