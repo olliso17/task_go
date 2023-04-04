448019cfc464089c4374eb509cb54d86ab274c72
Patricia Silva <patriciasilva@deso-se.com.br>
2023-04-04 11:15:45 -0300

    feat(beck_end):update entity cookie

--- front_end/.next/static/chunks/webpack.js
+++ front_end/.next/static/chunks/webpack.js
@@ -166,7 +166,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a43d030529985d25"; }
+/******/ 		__webpack_require__.h = function() { return "c41986b98934621c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
