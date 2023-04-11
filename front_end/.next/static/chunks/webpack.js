2403313dd700563f46b538ff7c19082347255c04
Patricia <olli.so17@gmail.com>
2023-04-10 21:24:53 -0300

    feat(back_end):delete cookie

--- front_end/.next/static/chunks/webpack.js
+++ front_end/.next/static/chunks/webpack.js
@@ -166,7 +166,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "85e87c10c639c886"; }
+/******/ 		__webpack_require__.h = function() { return "5ed52b1d23752a47"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
